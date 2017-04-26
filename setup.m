c5febcd570921d10d713699750805029be75f8dc
Alain Plattner <alainplattner@gmail.com>
2017-04-26 16:26:06 -0700

    Made setup octave compatible

--- setup.m
+++ setup.m
@@ -3,7 +3,7 @@ function setup()
 disp('Downloading external m-files from Luca Baradello website for reading PulseEkko Pro files')
 
 mkdir('ext');
-websave('ext/dt1read.m','http://www.lucabaradello.it/files/dt1read.m');
-websave('ext/dt1struct.m','http://www.lucabaradello.it/files/dt1struct.m');
-websave('ext/headerstruct.m','http://www.lucabaradello.it/files/headerstruct.m');
-websave('ext/dimstruct.m','http://www.lucabaradello.it/files/dimstruct.m');
+urlwrite('http://www.lucabaradello.it/files/dt1read.m','ext/dt1read.m');
+urlwrite('http://www.lucabaradello.it/files/dt1struct.m','ext/dt1struct.m');
+urlwrite('http://www.lucabaradello.it/files/headerstruct.m','ext/headerstruct.m');
+urlwrite('http://www.lucabaradello.it/files/dimstruct.m','ext/dimstruct.m');
