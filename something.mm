6974c2df4c9211a8c8ee64acd0bda09b8dba823e
Austin Robbins <austin.r.robbins@gmail.com>
2017-06-02 08:45:15 -0700

    deleted something.mm

--- something.mm
+++
@@ -1,9 +0,0 @@
-function setup()
-
-disp('Downloading external m-files from Luca Baradello website for reading PulseEkko Pro files')
-
-mkdir('ext');
-urlwrite('http://www.lucabaradello.it/files/dt1read.m','ext/dt1read.m');
-urlwrite('http://www.lucabaradello.it/files/dt1struct.m','ext/dt1struct.m');
-urlwrite('http://www.lucabaradello.it/files/headerstruct.m','ext/headerstruct.m');
-urlwrite('http://www.lucabaradello.it/files/dimstruct.m','ext/dimstruct.m');
