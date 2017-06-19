e47506085eb5ccea2bcd0d993d5c2539bf8adc61
Alain Plattner <alainplattner@gmail.com>
2017-06-19 16:41:46 -0400

    wrote a new and beautiful makeElev which can work with all sorts of topo data structures

--- tools/makeElev.m
+++ tools/makeElev.m
@@ -1,57 +1,76 @@
-function elev=makeElev(elevdat,xpos,ylines,data,surveyparams)
-% elev=makeElev(elevdat,xpos,ylines,data,surveyparams)
+function elev=makeElev(elevdat,xpos,data,surveyparams)
+% elev=makeElev(elevdat,xpos,data,surveyparams)
 %
-% Returns an interpolated topography matrix for all the data points for all
-% the parallel lines
+% Returns an interpolated topography matrix for all the data points
+% assuming that each profile is parallel to the x axis.
+% The first profile has y-coordinate = 0,
+% the second one y = surveyparams.lineincr,
+% the third one y = 2 * surveyparams.lineincr
+%
+% If lineincr is a vector of length surveyparams.nmorelines, then the
+% k-th line has y-coordinate sum(surveyparams.lineincr(k-1)) 
 %
 % INPUT:
 %
-% elevdat       Matrix containing the measured topography for some lines 
-%               at given x position
-% xpos          Vector containing the x locations for topography for the 
-%               given lines
-% ylines        line numbers
+% elevdat       if data is a single line: Vector containing elevations
+%               if data is several lines: Cell array containing
+%                  elevations  
+%               if randomly spaced areal points: n x 3 matrix containing
+%                  x, y, elev positions (no extrapolation)
+% xpos          if data is a single line: Vector containing x positions
+%                  of elevation measurements
+%               if data is several lines: Cell array containing
+%                  x positions  
+%               if randomly spaced areal points: []
 % data          Your loaded GPR data
 % surveyparams  Your preset survey parameters
 %
 % OUTPUT:
 %
 % elev          matrix containing the interpolated topography points for
-%               each GPR data point
+%               each GPR data point. To be used with elevCorrect
 %
-% Last modified by plattner-at-alumni.ethz.ch, 05/19/2015
+% Last modified by plattner-at-alumni.ethz.ch, 06/19/2017
 
-%if size(elevdat,1)~=size(data.gprdata,3) | size(elevdat,2)~=length(data.finalex)
-%    error('Lines of elevdat matrix must be x positions, columns must be lines')
-    %error('Your elevdat must be a struct! for example elevdat{1}=[points]')
-%end   
-    
-if xpos(1)>data.finalex(1) | xpos(end)<data.finalex(end)
-    warning('Extrapolation not allowed here. Elevation values will be NaN')
-end
+if iscell(elevdat)
+  % GPS points for individual parallel lines
+  if ~iscell(xpos)
+    error('if elevdat is a cell array, then xpos must be a cell array too')
+  end
 
-% determine if 1d or 2d interpolation
-if size(elevdat,1)==1 | size(elevdat,2)==1
-    
-    elev = (interp1(xpos,elevdat,data.finalex,'pchip'))';
-    % pchip is much better because with spline, if you go from flat to
-    % steep, you do a little undulation. This one doesn't do this.
-    
-    
+  % Do the simple 1-D interpolation for each line
+  elev=nan(length(elevdat),length(data.finalex));  
+  for i=1:length(elevdat)
+      elev(i,:)=(interp1(xpos{i},elevdat{i},data.finalex,'pchip','extrap'));
+  end
+  % Need to transpose for easier use in ElevCorrect
+  elev=elev';    
+  
 else
-            
-    if length(xpos)==size(elevdat,1) | length(ylines)==size(elevdat,2)
-        elevdat=elevdat';
-    end            
-        
-    [XX,YY]=meshgrid(xpos,surveyparams.lineincr*ylines);
-    elev=interp2(XX,YY,elevdat,...
-        data.finalex',surveyparams.lineincr*(0:(length(data.gprcell)-1)),...
-        'cubic');
-                
+% Either randomly distributed points, or a single line.
+  if isempty(xpos)
+    % it's randomly distributed points
+    if length(surveyparams.lineincr)==1
+      % Lines are equidistant
+      ygridvals=(0:1:surveyparams.nmorelines)...
+	    *surveyparams.lineincr;
+    else
+      % lineincr gives each line increment
+      ygridvals=cumsum([0 surveyparams.lineincr]);
+    end
+    
+    xgridvals=data.finalex;
+    [XX,YY]=meshgrid(xgridvals,ygridvals);
+    elev=griddata(elevdat(:,1),elevdat(:,2),elevdat(:,3),XX,YY,'linear');
+    % Need to transpose for easier use in ElevCorrect
     elev=elev';
     
+  else
+    % It's 1-d
+    elev = (interp1(xpos,elevdat,data.finalex,'pchip','extrap'))';
+    % pchip is much better because with spline, if you go from flat to
+    % steep, you do a little undulation. This one doesn't do this.    
+  end
+
 end
-    
-    
-    
+
